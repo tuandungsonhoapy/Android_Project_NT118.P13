f7105766d5fab327cef950619cd33bcae98c164c
trieu1212 <lequoctrieu618@gmail.com>
2024-10-28 13:03:30 +0700

    add category

--- app/src/main/java/com/example/androidproject/features/admin_manager/presentation/AdminCategoryManagerActivity.java
+++ app/src/main/java/com/example/androidproject/features/admin_manager/presentation/AdminCategoryManagerActivity.java
@@ -1,9 +1,11 @@
 package com.example.androidproject.features.admin_manager.presentation;
 
+import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +19,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidproject.R;
+import com.example.androidproject.features.admin_manager.category_manager.presentation.AddCategoryActivity;
 import com.example.androidproject.features.admin_manager.category_manager.presentation.ListCategoryItemAdminAdapter;
 import com.example.androidproject.features.category.usecase.CategoryUseCase;
 
 public class AdminCategoryManagerActivity extends AppCompatActivity {
     private RecyclerView rvCategoryList;
+    private Button btnAddCategory;
     private CategoryUseCase categoryUseCase = new CategoryUseCase();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,12 @@ public class AdminCategoryManagerActivity extends AppCompatActivity {
         rvCategoryList = findViewById(R.id.recycler_categories_view);
         rvCategoryList.setAdapter(new ListCategoryItemAdminAdapter(categoryUseCase.getCategoryList(), this));
         rvCategoryList.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAddCategory = findViewById(R.id.btnAddCategory);
+        btnAddCategory.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddCategoryActivity.class);
+            startActivity(intent);
+        });
     }
 
     @Override
