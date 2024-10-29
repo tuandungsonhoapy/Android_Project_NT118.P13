885a1e3d821e39c89ef1fd1a1d0410718bb1abe2
DungDev <tuandungsonhoapy@gmail.com>
2024-10-29 20:32:22 +0700

    complete code product detail screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.constraintlayout)
     implementation(libs.mediarouter)
     implementation(libs.navigation.fragment)
