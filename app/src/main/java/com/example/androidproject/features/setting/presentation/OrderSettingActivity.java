9cdc3e1b08cc952759032dc647ccf7e6cd4910c3
DungDev <tuandungsonhoapy@gmail.com>
2024-10-30 13:21:48 +0700

    complete code order history screen

--- app/src/main/java/com/example/androidproject/features/setting/presentation/OrderSettingActivity.java
+++ app/src/main/java/com/example/androidproject/features/setting/presentation/OrderSettingActivity.java
@@ -2,6 +2,9 @@ package com.example.androidproject.features.setting.presentation;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidproject.R;
+import com.example.androidproject.features.order.presentation.OrderHistoryListAdapter;
+import com.example.androidproject.features.order.usecase.OrderUseCase;
+
+import java.util.List;
 
 public class OrderSettingActivity extends AppCompatActivity {
+    private Spinner spinner_order_status;
+    private RecyclerView recycler_order_list;
+    private OrderUseCase orderUseCase = new OrderUseCase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,21 @@ public class OrderSettingActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        initView();
+    }
+
+    public void initView() {
+        spinner_order_status = findViewById(R.id.spStatus);
+        recycler_order_list = findViewById(R.id.recycler_order_history);
+
+        List<String> statusList = List.of("Tất cả", "Đang xử lý", "Đang giao", "Thành công", "Thất bại");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, statusList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_order_status.setAdapter(adapter);
+
+        recycler_order_list.setLayoutManager(new LinearLayoutManager(this));
+        recycler_order_list.setAdapter(new OrderHistoryListAdapter(this, orderUseCase.getAllOrders()));
     }
 
     @Override
