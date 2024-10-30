604a335f7b2765e89dea5f800379588f3c6a5006
trieu1212 <lequoctrieu618@gmail.com>
2024-10-30 21:37:15 +0700

    all product ui

---
+++ app/src/main/java/com/example/androidproject/core/utils/ItemData.java
@@ -0,0 +1,32 @@
+package com.example.androidproject.core.utils;
+
+public class ItemData {
+    private String id;
+    private String name;
+
+    public ItemData(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
